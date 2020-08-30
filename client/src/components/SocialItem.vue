603fe992d4b4d871c7dc6ab1cd5b07eb60257441
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-08-30 17:56:11 +0200

    :iphone: responsive(about-me): create about-me page

---
+++ client/src/components/SocialItem.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="social-item">
+		<div class="icon">
+			<a :href="target" rel="nofollow noopener noreferrer" target="_blank">
+				<i :class="'icon-' + icon" :style="'color:' + color"></i>
+			</a>
+		</div>
+		<p v-if="label" class="label">{{ label }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SocialItem',
+	props: ['icon', 'color', 'label', 'target'],
+}
+</script>
+
+<style lang="scss" scoped>
+.social-item {
+	.icon {
+		width: 50px;
+		height: 50px;
+		position: relative;
+		i {
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			transform: translate(-50%, -50%);
+			font-size: 40px;
+		}
+	}
+}
+</style>
--- client/src/template/pages/about-me.vue
+++ client/src/template/pages/about-me.vue
@@ -1,13 +1,32 @@
 <template>
 	<div class="about-me">
 		<div class="header">
-			<img src="https://via.placeholder.com/1000x1000" class="img-fluid" alt="avatar" />
+			<div class="banner">
+				<img src="https://via.placeholder.com/1000x1000" class="img-fluid" alt="avatar" />
+			</div>
 			<div class="me">
-				<h1>Baptiste Parmantier</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae incidunt quo beatae iste saepe doloremque tempora deleniti nesciunt sunt fugit, illum accusantium consectetur quis? Suscipit quas neque reprehenderit eligendi.</p>
+				<div class="container">
+					<h1>Baptiste Parmantier</h1>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae incidunt quo beatae iste saepe doloremque tempora deleniti nesciunt sunt fugit, illum accusantium consectetur quis? Suscipit quas neque reprehenderit eligendi.</p>
+					<div class="social-container">
+						<social-item
+							icon="linkedin"
+							color="#0073B2"
+							target="https://www.linkedin.com/in/baptiste-parmantier/"
+						/>
+						<social-item
+							icon="facebook-square"
+							color="#004AAF"
+							target="https://www.facebook.com/parmantier.baptiste/"
+						/>
+						<social-item icon="twitter" color="#1DA1F2" target="https://twitter.com/LeadcodeDev" />
+						<social-item icon="github" color="#24292E" target="https://github.com/LeadcodeDev" />
+						<social-item icon="discord" color="#7B8DCF" />
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="container">
+		<div class="cards-container">
 			<div v-for="(n, key) in 6" class="card" :key="key">
 				<img src="https://via.placeholder.com/1920x1080" class="img-fluid" alt="avatar" />
 				<div class="card-body">
@@ -20,12 +39,17 @@
 </template>
 
 <script>
+import SocialItemVue from '../../components/SocialItem.vue'
 export default {
 	layout: 'master',
+	components: {
+		'social-item': SocialItemVue,
+	},
 }
 </script>
 
 <style lang="scss" scoped>
+$avatarWidth: 200px;
 .about-me {
 	min-height: 100vh;
 	margin-bottom: calc(81.19px + 1rem);
@@ -35,18 +59,84 @@ export default {
 	.header {
 		display: flex;
 		flex-wrap: wrap;
-		width: 90vw;
+		width: 100vw;
 		margin: auto;
-		img {
-			padding: 2rem;
-			border-radius: 50%;
+		@media screen and (min-width: 576px) {
+			flex-wrap: nowrap;
+			justify-content: center;
+			padding: 3rem 0;
+		}
+		.banner {
+			background: #1e202a;
+			position: relative;
+			min-height: 20vh;
+			width: 100%;
+			@media screen and (min-width: 576px) {
+				display: flex;
+				background: initial;
+				width: fit-content;
+			}
+
+			img {
+				border-radius: 50%;
+				width: $avatarWidth;
+				position: absolute;
+				bottom: -$avatarWidth;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				@media screen and (min-width: 576px) {
+					width: 250px;
+					height: 250px;
+					position: initial;
+					left: 0;
+					transform: translate(0);
+					align-self: center;
+					margin-right: 2rem;
+				}
+			}
 		}
 		.me {
+			padding-top: $avatarWidth / 1.8;
+			text-align: justify;
+			@media screen and (min-width: 576px) {
+				align-self: center;
+				width: 30%;
+			}
+			h1 {
+				margin-bottom: 1.5rem;
+				@media screen and (min-width: 576px) {
+					margin-bottom: 3rem;
+				}
+			}
+			.social-container {
+				display: inline-flex;
+				justify-content: center;
+				width: 100%;
+				@media screen and (min-width: 576px) {
+					justify-content: left;
+				}
+				.social-item {
+					margin: 5px;
+					&:first-child {
+						margin-left: 0;
+					}
+				}
+			}
 		}
 	}
-	.container {
+	.cards-container {
+		margin-top: 4rem;
+		@media screen and (min-width: 576px) {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
+		}
 		.card {
 			margin: 1rem;
+			text-align: justify;
+			@media screen and (min-width: 576px) {
+				width: 25%;
+			}
 		}
 	}
 }
